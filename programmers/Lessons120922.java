5d048555594f9f125fef306279e9412075e8ce66
fivewincow <fivewincow@gmail.com>
2023-01-04 21:57:23 +0900

    종이 자르기

---
+++ programmers/Lessons120922.java
@@ -0,0 +1,8 @@
+// 종이 자르기
+// https://school.programmers.co.kr/learn/courses/30/lessons/120922
+public class Lessons120922 {
+    public int solution(int M, int N) {
+        int answer = M * N - 1;
+        return answer;
+    }
+}
