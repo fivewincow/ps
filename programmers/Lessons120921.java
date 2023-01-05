7b9082dbaf58819b2492f104f619c0654684361b
fivewincow <fivewincow@gmail.com>
2023-01-05 22:02:06 +0900

    문자열 밀기

---
+++ programmers/Lessons120921.java
@@ -0,0 +1,11 @@
+// 문자열 밀기
+// https://school.programmers.co.kr/learn/courses/30/lessons/120921
+public class Lessons120921 {
+    public int solution(String A, String B) {
+        int answer = (B + B).indexOf(A);
+        return answer;
+    }
+
+    // indexOf(String str)
+    // 특정 문자나 문자열이 앞에서 부터 처음 발견되는 인덱스를 반환 없으면 -1 반환
+}
